1051dd99bd7738233a875a0535ad503a27231753
hardtrip <jeremydumont.dumont@gmail.com>
2023-02-17 16:00:23 -0500

    MinorFirstMicrogameUpdate

--- scripts/TestMicrogame.gd
+++ scripts/TestMicrogame.gd
@@ -41,6 +41,8 @@ func _input(event: InputEvent) -> void:
 	if mouse_over and event.is_action_pressed("click"):
 		current_step += 1
 		animation.play("step" + str(current_step))
+	elif event.is_action_pressed("click"):
+		_on_timer_timeout()
 
 func _on_mouse_entered() -> void:
 	mouse_over = true
